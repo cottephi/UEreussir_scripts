e798a498b136d7654f3ce5e8d60e560702e5283b
Quentin <quentin.lieumont@gmail.com>
2018-07-20 10:51:13 +0200

    Power of ten
    Other way to create distractors in power_of_ten.py

--- powers_of_ten.py
+++ powers_of_ten.py
@@ -20,25 +20,6 @@ for i in range(0,M):
   formula = formula + str(c[i])+"\\cdot 10^{"+str(d[i])+"}\\times "
 formula = formula[:-7]+"}"
 
-prefacteur = fractions.Fraction(np.array(a).prod(), np.array(c).prod())
-exposant = np.array(b).sum() - np.array(d).sum()
-exposant_wrong = np.array(d).sum() - np.array(b).sum()
-answer = ""
-distractor3 = ""
-distractor2 = ""
-distractor1 = ""
-if prefacteur.denominator != 1:
-  answer = "$\\frac{"+str(prefacteur.numerator)+"}{"+str(prefacteur.denominator)+"}\\cdot 10^{"+str(exposant)+"}$"
-  distractor3 = "$\\frac{"+str(prefacteur.numerator)+"}{"+str(prefacteur.denominator)+"}\\cdot 10^{"+str(exposant_wrong)+"}$"
-else:
-  answer = "$"+str(prefacteur.numerator)+"\\cdot 10^{"+str(exposant)+"}$"
-  distractor3 = "$"+str(prefacteur.numerator)+"\\cdot 10^{"+str(exposant_wrong)+"}$"
-if prefacteur.numerator != 1:
-  distractor1 = "$\\frac{"+str(prefacteur.denominator)+"}{"+str(prefacteur.numerator)+"}\\cdot 10^{"+str(exposant)+"}$"
-  distractor2 = "$\\frac{"+str(prefacteur.denominator)+"}{"+str(prefacteur.numerator)+"}\\cdot 10^{"+str(exposant_wrong)+"}$"
-else:
-  distractor1 = "$"+str(prefacteur.denominator)+"\\cdot 10^{"+str(exposant)+"}$"
-  distractor2 = "$"+str(prefacteur.denominator)+"\\cdot 10^{"+str(exposant_wrong)+"}$"
 
 question = "Que vaut $"+formula+"$?"
 answers = [answer, distractor1, distractor2, distractor3]
